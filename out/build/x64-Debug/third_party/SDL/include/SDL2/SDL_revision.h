0007698916f8c7e49188f863abfaf4db417bb550
vishwah13 <vishwahsivagurunathan@gmail.com>
2025-07-12 16:03:08 +0530

    Remove build directories from tracking

--- out/build/x64-Debug/third_party/SDL/include/SDL2/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.28.4-g16fdca6 (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-2.28.4-g16fdca6"
-#endif
